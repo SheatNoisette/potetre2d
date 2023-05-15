c61f5b3fcd1443f63cb5f7f83236559a2a99f134
SheatNoisette <sheatnoisette@protonmail.com>
2023-05-15 19:23:10 +0200

    loader: file compression support

--- src/loader.c
+++ src/loader.c
@@ -19,10 +19,10 @@ char *pe_load_code(const char *path) {
     // Load a Wren file from the disk (or use default)
     if (path == NULL) {
         LOG_DEBUG("Loading default Wren file '%s'\n", DEFAULT_WREN_FILE);
-        wren_file_content = util_read_file(DEFAULT_WREN_FILE);
+        wren_file_content = find_and_load_decompress_file(DEFAULT_WREN_FILE);
     } else {
         LOG_DEBUG("Loading Wren file '%s'\n", path);
-        wren_file_content = util_read_file(path);
+        wren_file_content = find_and_load_decompress_file(path);
     }
 
     // Add stdlib
@@ -55,28 +55,44 @@ char *find_and_load_decompress_file(const char *path) {
     // Compressed file name (with '_' prefix and '\0')
     char *compressed_file_name = malloc(sizeof(char) * (path_length + 2));
     CHECK_ALLOC(compressed_file_name);
-    memset(compressed_file_name, 0, path_length + 1);
+    memset(compressed_file_name, 0, path_length + 2);
     compressed_file_name[0] = '_';
     memcpy(compressed_file_name + 1, path, path_length);
 
     // Check if a compressed file exists
     LOG_DEBUG("* Looking for compressed file '%s'...\n", compressed_file_name);
     if (util_file_exists(compressed_file_name) == true) {
-        LOG_DEBUG("- Found compressed file '%s'\n", compressed_file_name);
+        LOG_DEBUG(" - Found compressed file '%s'!\n", compressed_file_name);
+
         file_content = util_read_file(compressed_file_name);
         size_t compressed_file_size = util_file_length(compressed_file_name);
+
+        LOG_DEBUG(" - File read! Decompressing file '%s'...\n",
+                  compressed_file_name);
+
         // Decompress the file {
         size_t output_size = 0;
-        pithy_GetDecompressedLength(file_content, compressed_file_size, &output_size);
-        char *decompressed_file_content = malloc(sizeof(char) * (compressed_file_size + 1));
+        char *decompressed_file_content = NULL;
+
+        pithy_GetDecompressedLength(file_content, compressed_file_size,
+                                    &output_size);
+
+        decompressed_file_content = calloc(sizeof(char), output_size);
         CHECK_ALLOC(decompressed_file_content);
-        pithy_Decompress(file_content, compressed_file_size, decompressed_file_content, output_size);
+
+        pithy_Decompress(file_content, compressed_file_size,
+                         decompressed_file_content, output_size);
         // }
+
         // Compressed file content is no longer needed
-        LOG_DEBUG("- Decompressed file size: %lu from %lu\n", output_size, compressed_file_size);
+        LOG_DEBUG(" - File unpacked! File size: %lu from %lu (ratio: %lu)\n",
+                  output_size, compressed_file_size,
+                  compressed_file_size * 100 / output_size);
+
         free(file_content);
+        file_content = decompressed_file_content;
     } else {
-        LOG_DEBUG("Found not compressed file '%s'\n", path);
+        LOG_DEBUG(" - Compressed file not found, using '%s' instead\n", path);
         file_content = util_read_file(path);
     }
 
